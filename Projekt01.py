81e04b926b60bc2ce528d36d5a96c2af33a9858e
M4T30-772 <75365956+M4T30-772@users.noreply.github.com>
2023-01-09 01:13:30 +0100

    Add files via upload
    database spojen ........................

--- Projekt01.py
+++ Projekt01.py
@@ -1,33 +1,54 @@
-import flask
+from pymongo import MongoClient
 import json
 
-usernameFinder = 'username = \''
+uri = "mongodb+srv://Mateo:Mateo@projekt1.vmoiu28.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri)
+db = client.sample_mflix
+
+#kako uzet prvih sto?
+def usernameW(data):
+    usernameFinder = 'username = \''
+    usernames = []
+    for i in data:
+        if i['content']:
+            z = i['content']
+            singleContent = z.find(usernameFinder)
+            if singleContent != -1:
+                usernameStart = singleContent + len(usernameFinder)
+                completeUsername = z[usernameStart:].partition("'")[0]
+                if completeUsername[0] == 'w':
+                    usernames.append(completeUsername)
+    return usernames  # return the list of usernames
+
+
+#kako uzet prvih sto?
+def usernameD(data):
+    usernameFinder = 'username = \''
+    usernames = []
+    for i in data:
+        if i['content']:
+            z = i['content']
+            singleContent = z.find(usernameFinder)
+            if singleContent != -1:
+                usernameStart = singleContent + len(usernameFinder)
+                completeUsername = z[usernameStart:].partition("'")[0]
+                if completeUsername[0] == 'd':
+                    usernames.append(completeUsername)
+    return usernames  # return the list of usernames
+
+#-----------------------------------------------------------------------------
 file = open('test1.json')
 data = json.load(file)
-for i in data:
-    if i['content']:
-        z = i['content']
-        singleContent = z.find(usernameFinder)
-        if singleContent != -1:
-            usernameStart = singleContent + len(usernameFinder)
-            completeUsername = z[usernameStart:].partition("'")[0]
-            if completeUsername[0] == 'w':
-                print(completeUsername)
+usernameW(data)
+usernameD(data)
 
-usernameFinder = 'username = \''
-file = open('test1.json')
-data = json.load(file)
-for i in data:
-    if i['content']:
-        z = i['content']
-        singleContent = z.find(usernameFinder)
-        if singleContent != -1:
-            usernameStart = singleContent + len(usernameFinder)
-            completeUsername = z[usernameStart:].partition("'")[0]
-            if completeUsername[0] == 'd':
-                print(completeUsername)
+usernames_w = usernameW(data)
+print(usernames_w)
+usernames_d = usernameD(data)
+print(usernames_d)
 
-
+db.users.insert_many([{"username": username} for username in usernames_w])
+db.users.insert_many([{"username": username} for username in usernames_d])
         
 #app = Flask(__name__)
 
